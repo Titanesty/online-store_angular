a85c3d9f3583732e35bf7304349c7d6f3c0ff1af
user <kovalev.fortech@gmail.com>
2023-03-29 18:02:15 +0300

    feature: third day

--- src/app/components/the-pag/the-pag.component.ts
+++ src/app/components/the-pag/the-pag.component.ts
@@ -1,8 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "ThePag",
   templateUrl: "./the-pag.component.html",
   styleUrls: ["./the-pag.component.scss"],
 })
-export class ThePagComponent {}
+export class ThePagComponent {
+  @Input() allPages: number = 0;
+}
